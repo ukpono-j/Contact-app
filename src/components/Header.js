428bcd7a7180ead86ad8e2c2efb1fa442776a25a
ukpono-j <ukponoakpan270@gmail.com>
2021-06-28 22:44:55 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+// export default function Header(props) {
+//     return (
+//         <div className = "ui fixed menu">
+//             <div className = "ui container center">
+//                 <h2>Contact Management</h2>
+//             </div>
+//         </div>
+//     );
+// }
+function Header(props){
+   return(
+       <div className = "ui fixed menu">
+           <div className = "ui container center">
+               <h2>Contact Management</h2>
+           </div>
+       </div>
+   )
+}
+export default Header
